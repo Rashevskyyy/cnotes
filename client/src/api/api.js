07f854e19f56923eac18f5476dea464b9698bbf6
Rashevskyyy <keepcalm312000@gmail.com>
2023-11-01 06:40:48 +0100

    change api

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_PROD_API_URL,
 });
 
 api.interceptors.request.use((config) => {
